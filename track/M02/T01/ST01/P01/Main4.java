a3e806ccf74bffe3dfa7bee632d5f2ce5829de03
Sravya <sravyaemmadi03@gmail.com>
2026-08-04 12:28:58 +0530

    Homework02

---
+++ track/M02/T01/ST01/P01/Main4.java
@@ -0,0 +1,12 @@
+public class Main4 {
+    public static void main(String[] args) {
+        int num = 24;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}

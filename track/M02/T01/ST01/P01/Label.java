13d8a38795838b6185d2c4ce4d15ea0a03894437
Sravya <sravyaemmadi03@gmail.com>
2026-08-06 12:58:59 +0530

    continue

--- track/M02/T01/ST01/P01/Label.java
+++ track/M02/T01/ST01/P01/Label.java
@@ -1,9 +1,9 @@
-public class Nestedpgm2 {
+public class Label {
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 4; j++) {
+        outer: for (int i = 1; i <= 3; i++) {
+            inner: for (int j = 1; j <= 4; j++) {
                 if (i == 2) {
-                    break;
+                    break outer;
                 }
                 System.out.println("i: " + i + "j: " + j);
             }

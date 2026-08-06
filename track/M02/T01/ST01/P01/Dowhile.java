0cedf3073230fe8145e388e9e532e1fdc4f1b450
Sravya <sravyaemmadi03@gmail.com>
2026-08-06 11:41:45 +0530

    do ehile

---
+++ track/M02/T01/ST01/P01/Dowhile.java
@@ -0,0 +1,16 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.print(j);
+                j++;
+            } while (j <= 5);
+
+            System.out.println();
+            i++;
+        } while (i <= 5);
+    }
+
+}

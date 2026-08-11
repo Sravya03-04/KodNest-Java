0ba374ebb8627d754967ebf81f35583bddabcf2c
Sravya <sravyaemmadi03@gmail.com>
2026-08-11 12:02:29 +0530

    classemployee

---
+++ track/Practice/M01/T01/Main13.java
@@ -0,0 +1,12 @@
+class Robot {
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class Main13 {
+    public static void main(String[] args) {
+        Robot r1 = new Robot();
+        r1.speak();
+    }
+}

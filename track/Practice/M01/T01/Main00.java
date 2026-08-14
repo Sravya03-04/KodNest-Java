4365ebab2f2714436298d94ad7616d0397fe066b
Sravya <sravyaemmadi03@gmail.com>
2026-08-14 12:16:34 +0530

    Application

---
+++ track/Practice/M01/T01/Main00.java
@@ -0,0 +1,14 @@
+class Robot {
+    Robot() {
+        // Print the message
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class Main00 {
+    public static void main(String[] args) {
+        // Create one Robot object
+        Robot robot = new Robot();
+
+    }
+}

5d1aceb3129d0e3f6d01852fb438e679627b5631
Sravya <sravyaemmadi03@gmail.com>
2026-09-07 17:29:00 +0530

    String4

---
+++ track/Strings/pgm4.java
@@ -0,0 +1,12 @@
+public class pgm4 {
+    public static void main(String[] args) {
+        String s3 = "java";
+        String s4 = "java";
+        if (s3.equals(s4)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+
+    }
+}
